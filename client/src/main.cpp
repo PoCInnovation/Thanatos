59761cb893d770dcc42729787766ce1fe35fa4f7
Nestyles <tom.sancho@epitech.eu>
2022-06-14 00:47:12 +0200

    [ADD] Example of usage of the send_message method

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -8,41 +8,44 @@
 
 int connect_to_server(const std::string ip, const int port)
 {
-    int sockfd, connfd;
-	struct sockaddr_in servaddr, cli;
+    int sockfd;
+    struct sockaddr_in servaddr;
 
-	// socket create and verification
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		printf("socket creation failed...\n");
-		exit(0);
-	}
-	else
-		printf("Socket successfully created..\n");
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
     bzero(&servaddr, sizeof(servaddr));
 
-	// assign IP, PORT
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr(ip.c_str());
-	servaddr.sin_port = htons(port);
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(ip.c_str());
+    servaddr.sin_port = htons(port);
 
-	// connect the client socket to server socket
-	if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
-		printf("connection with the server failed...\n");
-		exit(0);
-	}
-	else
-		printf("connected to the server..\n");
+    // connect the client socket to server socket
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
     return sockfd;
 }
 
 int main()
 {
     auto message = HttpMessage(HttpMessage::HttpMethod::GET, "0.0.0.0",
-        "/hello.html");
+        "/hello");
+    auto socket = connect_to_server("192.168.0.12", 4000);
 
     message << "Salut je suis le body";
-    message << "\nhaha salut";
-    message.set_header("cle", "haha pas clé haha");
+    message << "\n salut";
+    message.set_header("cle", "pas cle");
     message.print_message();
+    message.send_message(socket);
+    close(socket);
 }
