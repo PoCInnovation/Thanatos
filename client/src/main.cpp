3d7f89ae6e49ca4d7e22114762161473e46fb77b
degodenne <thibaut.degodenne@epitech.eu>
2022-06-13 17:15:49 +0200

    [FEAT] Create socket

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,6 +3,38 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <strings.h>
+#include <unistd.h>
+
+int connect_to_server(const std::string ip, const int port)
+{
+    int sockfd, connfd;
+	struct sockaddr_in servaddr, cli;
+
+	// socket create and verification
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		printf("socket creation failed...\n");
+		exit(0);
+	}
+	else
+		printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+	// assign IP, PORT
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr(ip.c_str());
+	servaddr.sin_port = htons(port);
+
+	// connect the client socket to server socket
+	if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
+		printf("connection with the server failed...\n");
+		exit(0);
+	}
+	else
+		printf("connected to the server..\n");
+    return sockfd;
+}
 
 int main()
 {
