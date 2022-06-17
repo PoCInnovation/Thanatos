86db8516f7b3376fe3ea80912989bb81294abba2
Nestyles <tom.sancho@epitech.eu>
2022-06-18 01:06:32 +0200

    [FIX] Parameters in the wrong order

--- client/src/connect.cpp
+++ client/src/connect.cpp
@@ -2,8 +2,8 @@
 
 void connect(int socket)
 {
-    HttpMessage connect_message(HttpMessage::HttpMethod::POST, "/connect",
-        "0.0.0.0");
+    HttpMessage connect_message(HttpMessage::HttpMethod::POST, "0.0.0.0",
+        "/connect");
 
     connect_message.send_message(socket);
 }
